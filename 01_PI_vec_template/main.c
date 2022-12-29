3d725e092438566719974537d42c3102269b3067
Bartosz <bartek.pawlowski00@gmail.com>
2022-12-29 12:37:40 +0100

    correct errors

--- 01_PI_vec_template/main.c
+++ 01_PI_vec_template/main.c
@@ -5,10 +5,9 @@
 // generates the sequence by incrementing the start value
 // using the step size until it reaches the stop value
 // (stop value is not included)
-int range(double array[], double start, double stop, double step)
-{
+int range(double array[], double start, double stop, double step) {
     int i = 0;
-    while ( ((start<stop) && (step>0) ) || ( (start>stop) && (step<0) )){
+    while ( ((start<stop) && (step>0)) || ((start>stop) && (step<0)) ) {
         array[i] = start;
         i++;
         start += step;
@@ -20,56 +19,49 @@ int range(double array[], double start, double stop, double step)
 // n >= 0
 // for n = 0 return empty array
 // for n = 1 return one-element array, with array[0] = start
-void linspace(double array[], double start, double stop, int n)
-{
-    if (n==0) return array;
-    if (n==1){
-        array[0] = start;
-        return array;
+void lin_space(double v[], double start, double stop, int n) {
+    if (n == 1) {
+        v[0] = start;
     }
-    double step;
-    step = (stop-start)/(n-1);
-    for (int i=0; i<n; i++){
-        array[i] = start;
-        start += step;
+    else if (n > 1) {
+        double step = (stop - start) / (n - 1);
+        for (int i = 0; i < n; i++) {
+            v[i] = start;
+            start += step;
+        }
     }
-    return array;
 }
 
 // multiply each element of v by the value of scalar
-void multiply_by_scalar(double v[], int n, double scalar)
-{
-    for (int i=0; i<n; i++){
+void multiply_by_scalar(double v[], int n, double scalar) {
+    for (int i = 0; i < n; i++) {
         v[i] *= scalar;
     }
-    return;
 }
 
 // add to each element v1[i] value of v2[i]
-void add(double v1[], const double v2[], int n)
-{   for (int i=0; i<n; i++) v1[i] += v2[i];
-    return;
+void add(double v1[], const double v2[], int n) {
+    for (int i = 0; i < n; i++) {
+        v1[i] += v2[i];
+    }
 }
 
 // calculate and return the dot product of v1 and v2
-double dot_product(const double v1[], const double v2[], int n)
-{
+double dot_product(const double v1[], const double v2[], int n) {
     double result = 0;
-    for (int i=0; i<n; i++){
-        result += v1[i] * v2[i];
+    for (int i = 0; i < n; i++) {
+        result += (v1[i] * v2[i]);
     }
     return result;
 }
 
 // read double vector of size n
-void read_vector(double v[], int n)
-{
-    double val;
-    for (int i=0; i<n; i++){
-        scanf("%lf",&val);
-        v[i] = val;
+void read_vector(double v[], int n) {
+    double value_read;
+    for (int i = 0; i < n; i++) {
+        scanf("%lf", &value_read);
+        v[i] = value_read;
     }
-    return;
 }
 
 // print double vector of size n (with 2 significant figures)
@@ -89,9 +81,9 @@ int main(void) {
 	scanf("%d", &to_do);
 
 	switch (to_do) {
-		case 1: // linspace
+		case 1: // lin-space
 			scanf("%d %lf %lf", &len, &start, &stop);
-			linspace(vector_1, start, stop, len);
+            lin_space(vector_1, start, stop, len);
 			print_vector(vector_1, len);
 			break;
 		case 2: // add
